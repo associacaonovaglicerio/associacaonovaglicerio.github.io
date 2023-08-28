b84e158d02582e0cdcf06b70c76711405a91fd8f
associacaonovaglicerio <associacaonovaglicerio@protonmail.com>
2023-08-28 16:26:33 -0300

    subindo base

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import logo from "./Images/nova_glicerio_LOGO_page-0001.jpg"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img style={{borderRadius: "70px"}} src={logo} className="App-logo" alt="logo" />
+        <p>
+          Em Construcao .
+        </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
