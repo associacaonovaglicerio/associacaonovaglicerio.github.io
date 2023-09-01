f70d85fa76142f4d08fb5c2cbe37321c8c163131
associacaonovaglicerio <associacaonovaglicerio@protonmail.com>
2023-09-01 10:23:14 -0300

    basename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App () {
     return (
         
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/associacaonovaglicerio' element={<Home />} />
             <Route exact path="/moedamudas" element={<MoedaMudas />} />
         </Routes>
         
