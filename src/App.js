ded64b5c952c2a198fcf4ae2bb79c15a496ed6f2
associacaonovaglicerio <associacaonovaglicerio@protonmail.com>
2023-09-01 10:28:31 -0300

    hash

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App () {
     return (
         
         <Routes>
-            <Route path='/associacaonovaglicerio' element={<Home />} />
+            <Route path='/' element={<Home />} />
             <Route exact path="/moedamudas" element={<MoedaMudas />} />
         </Routes>
         
